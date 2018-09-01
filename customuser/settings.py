67cf4408232000a39bb455aa9f936db7ede49a48
Jeffrey Chidi <jeffdev1111@gmail.com>
2018-09-01 02:06:28 +0100

    added custom 404

--- customuser/settings.py
+++ customuser/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!wt7lv13qd8c#^j!0@4)f&*c)3^+gu%dw0*728p340w=h(0l71'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jeffdevs.pythonanywhere.com']
 
 
 # Application definition
@@ -120,5 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 AUTH_USER_MODEL = 'app.MyUser'
